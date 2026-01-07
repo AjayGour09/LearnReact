38405d5bd8e4e3560a15c44adcfdd88925273c27
Ajay Gour <ajaygour369@gmail.com>
2026-01-07 22:33:41 +0530

    CountryProject

---
+++ CountryProject/src/Components/UI/Header.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom"
+export const Header = () =>{
+    return (
+        <>
+        <div className="flex items-center justify-around bg-gray-500 p-3 shadow">
+            <div>
+                <NavLink to="/">
+                    <h1 className="text-2xl text-amber-50">WorldTour</h1>
+                </NavLink>
+            </div>
+            <div>
+                <ul className="flex gap-12 ml-100 ">
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="about">About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="country">Country</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="contact">Contact</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </>
+    )
+}
---
+++ CountryProject/src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>This is Home Page</p>
+    </div>
+  );
+};
